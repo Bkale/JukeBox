7e58a563d4dadc90a1f038842e9bc636ece49451
Babajide Kale <bkale09@gmail.com>
2017-02-15 03:18:08 -0500

    build

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 (() => {
+  //Can be moved to another JS file
+  SC.initialize({
+    client_id: '7e6000504572d932b02e7f412acb437c'
+  });
+
   const submit = document.querySelector('.js-submit')
   const input = document.querySelector('.js-searchBar')
+  const container = document.querySelector('.js-content')
+  const playList = document.querySelector('.js-playlist')
 
 
 //Validates Search and Calls get
-  function validateSearch() {
+  async function validateSearch() {
 		const searchTerm = input.value;
 		if (searchTerm.trim() === "") {
 			alert('Please input a value!')
@@ -13,23 +20,47 @@
 		input.setAttribute('disabled', 'disabled');
 		submit.setAttribute('disabled', 'disabled');
 
-
-    spotifySearch(searchTerm)
+    await spotifySearch(searchTerm, renderTracks)
+    input.removeAttribute('disabled')
+    submit.removeAttribute('disabled')
 	}
 
-  function spotifySearch(searchTerm){
-    console.log(searchTerm);
+
+
+  function spotifySearch(searchTerm, callback){
+    SC.get('/tracks', {
+      q: searchTerm, license: 'cc-by-sa'
+    }).then(function(tracks) {
+      callback(tracks);
+    })
+
+  }
+  function renderTracks(tracks){
+    for(const track of tracks){
+      console.log(track);
+
+      const card = document.createElement('div')
+      card.classList.add('card')
+
+      const artwork = document.createElement('img')
+      artwork.classList.add('artwork')
+      track.artwork_url ? artwork.src = track.artwork_url : artwork.src = './assets/alt.jpg'
+      const fav = document.createElement('button')
+      fav.innerText = "Add To Favorites"
+
+      fav.addEventListener('click',() => addToPlaylist(track.id))
+
+      card.appendChild(artwork)
+      card.appendChild(fav)
+      container.appendChild(card)
+    }
   }
 
-
-
-
-
-
-
-
-
-
+  function addToPlaylist(songId){
+    playList.style.display = 'flex'
+    console.log(songId);
+    //Render Playlist
+  }
 
 
 //****************   ONCLICK   ********************
